94b18e7a0da942335f92da07c943d92acf0007db
meynard <sombritomeynard@gmail.com>
2025-08-06 09:08:26 +0800

    test: Pass 4/4 of debugging exercise rspec

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,3 +1,4 @@
+require "pry-byebug"
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -10,7 +11,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -24,7 +25,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -37,8 +38,12 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split("") #=> fix: provide a separator ("")
+  p string_array # ["odin"] => the string_array is not split
+  puts string_array.inspect
   unique_length = string_array.uniq.length
+  p unique_length # 1 => the test argument is "Odin", we should have 4
+  puts unique_length.inspect
   original_length == unique_length
 end
 
@@ -51,6 +56,11 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase #=> after reassignment, `name` is still in lower case
+                     # this one is obvious because #downcase method was used
+                     # fix: change the method to #upcase
+  # binding.pry
   greeting = "WASSAP, #{name}!"
 end
+
+puts yell_greeting("bob")

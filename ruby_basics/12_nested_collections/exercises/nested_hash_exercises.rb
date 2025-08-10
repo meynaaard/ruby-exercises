6f5b99f3802048e63b69f06e18a0fbe4d6ffde92
meynard <sombritomeynard@gmail.com>
2025-08-10 09:11:25 +0800

    test: Pass 2/2 of find beautiful languages exercise rspec

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -41,6 +41,7 @@ end
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
+  languages.select { |_, info| info[:is_beautiful?] }
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
